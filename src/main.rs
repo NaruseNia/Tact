47f4549427db71f5d7f0434e072cb2a5496b4587
NaruseNia <notshinsin0000@gmail.com>
2021-10-12 20:57:50 +0900

    Setup for simple REST server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod handlers;
+
+use actix_web::{ web, App, HttpServer }; 
+use std::io::Result;
+use std::env;
+use dotenv::dotenv;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+
+    //get env
+    dotenv().ok();
+
+    //get host and port from .env
+    let server_host = env::var("SERVER_HOST").unwrap_or("127.0.0.1".into());
+    let server_port = env::var("SERVER_PORT").unwrap_or("8080".into());
+
+    println!("Server started on {}:{}", &server_host, &server_port);
+
+    //Router
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(handlers::status))
+    })
+    .bind(format!("{}:{}", &server_host, &server_port))?
+    .run()
+    .await
 }
+
+
+
